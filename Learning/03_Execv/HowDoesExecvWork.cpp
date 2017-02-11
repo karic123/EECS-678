d1d880004fc70feb22883fdf66f5e87572dee5df
Rachel "Wil Sha" Morris <racheljmorris@gmail.com>
2017-02-11 17:38:57 -0600

    Working on the RachUtil file

--- Learning/03_Execv/HowDoesExecvWork.cpp
+++ Learning/03_Execv/HowDoesExecvWork.cpp
@@ -11,50 +11,48 @@ using namespace std;
 #include <cstring>
 #include <sys/wait.h>
 
+
 #define FIND_EXEC  "/usr/bin/find"
 #define PWD_EXEC  "/bin/pwd"
 
 #include "RachUtils.h"
 
-/* Parent       Child           execv
-    pipe         pipe           stdout
-    | <<<<<<<<<<< | <<<<<<<<<<<< |
-     in       out   in       out
-*/
 
 void ChildProgram( int childToParent_fd[2] )
 {
     // Duplicate - Child in << STDOUT
-	close( childToParent_fd[ READ_PIPE ] );
-    dup2( childToParent_fd[ WRITE_PIPE ], STDOUT_FILENO );
+	close( childToParent_fd[ READ_INPUT_PIPE ] );
+    dup2( childToParent_fd[ WRITE_OUTPUT_PIPE ], STDOUT_FILENO );
 
     // Execute the "pwd" command
     char* programName = FIND_EXEC;
     char* arguments[] = { programName, ".", NULL };
 
-    cout << "\t Child: Call exec" << endl;
-
     if ( execv( programName, arguments ) == -1 )
     {
-        cerr << "\t Child: Error using exec" << endl;
-        cout << GetExecvError( errno ) << endl;
+        PrintError();
     }
-
-    cout << "\t Child: This shouldn't happen" << endl;
-
 }
 
 void ParentProgram( int childToParent_fd[2] )
 {
 	// Close output channel - just getting input from child
-	close( childToParent_fd[ OUTPUT_PIPE ] );
+	close( childToParent_fd[ WRITE_OUTPUT_PIPE ] );
 
     char readBuffer[256];
 
-    int bytes = read( childToParent_fd[ INPUT_PIPE ], readBuffer, sizeof( readBuffer ) );
-    cout << "\t Parent: Received data: " << endl;
+    int bytes = read( childToParent_fd[ READ_INPUT_PIPE ], readBuffer, sizeof( readBuffer ) );
 
-    cout << endl << endl << readBuffer << endl;
+    if ( bytes > 0 )
+    {
+        cout << "Parent - Received data: " << endl << endl
+            << readBuffer << endl;
+    }
+    else
+    {
+        cout << "Parent - Did not receive any data!" << endl;
+    }
+
 }
 
 int main()
@@ -65,12 +63,12 @@ int main()
 
 	// Create a child process to execute a task
 	pid_t pid = fork();
-	if ( pid == FORK_ERROR )
+
+	if ( IsError( pid ) )
 	{
-		cerr << "Error with fork()" << endl;
-		return FORK_ERROR;
+		return PrintError();
 	}
-	else if ( pid == CHILD_PROCESS )
+	else if ( IsChild( pid ) )
 	{
 		ChildProgram( childToParent_fd );
 	}
--- Learning/03_Execv/RachUtils.h
+++ Learning/03_Execv/RachUtils.h
@@ -1,36 +1,31 @@
 #ifndef _RACHELS_UTILS_HPP
 #define _RACHELS_UTILS_HPP
 
+#include <stdio.h>
+#include <string.h>
+
 /*
     write               read
     output <<< PIPE <<< input
 */
 
-const int INPUT_PIPE 	= 0;
-const int OUTPUT_PIPE 	= 1;
+const int READ_INPUT_PIPE = 0;
+const int WRITE_OUTPUT_PIPE = 1;
 
-const int READ_PIPE 	= 0;
-const int WRITE_PIPE 	= 1;
-
-const int CHILD_PROCESS = 0;
-const int FORK_ERROR = -1;
-
-char* GetExecvError( int code )
+bool IsError( pid_t pid )
 {
-    switch( code )
-    {
-        case ENODEV:    return "ENODEV		The device prefix of program did not exist.";    break;
-        case ENOTDIR:   return "ENOTDIR		A non-final component of program was not a directory.";    break;
-        case ENOENT:    return "ENOENT		program did not exist.";    break;
-        case EISDIR:    return "EISDIR		program is a directory.";    break;
-        case ENOEXEC:   return "ENOEXEC		program is not in a recognizable executable file format, was for the wrong platform, or contained invalid fields.";    break;
-        case ENOMEM:    return "ENOMEM		Insufficient virtual memory is available.";    break;
-        case E2BIG:     return "E2BIG		The total size of the argument strings is too large.";    break;
-        case EIO:       return "EIO		A hard I/O error occurred.";    break;
-        case EFAULT:    return "EFAULT		One of the args is an invalid pointer.";    break;
-        default:        return "Unknown error code"; break;
+    return ( pid == -1 );
+}
 
-    }
+bool IsChild( pid_t pid )
+{
+    return ( pid == 0 );
+}
+
+int PrintError()
+{
+    printf( "\n ERROR! \t %s \n", strerror( errno ) );
+    return errno;
 }
 
 #endif
