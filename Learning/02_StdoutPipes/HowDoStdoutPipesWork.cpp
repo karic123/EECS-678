b32f61dfc805c019040044ce38c9104690597c04
Rachel "Wil Sha" Morris <racheljmorris@gmail.com>
2017-02-11 17:07:53 -0600

    Child process writes to stdout, and dupe also sends that info to the parent process

---
+++ Learning/02_StdoutPipes/HowDoStdoutPipesWork.cpp
@@ -0,0 +1,79 @@
+/* Reference:
+ * http://tldp.org/LDP/lpg/node10.html#SECTION00721000000000000000
+ * http://tldp.org/LDP/lpg/node11.html
+ *
+ * "To send data to the pipe, we use the write() system call,
+ * and to retrieve data from the pipe, we use the read() system call."
+ * */
+
+// Happy C++ things
+#include <iostream>
+using namespace std;
+
+#include <unistd.h>
+#include <cstring>
+#include <sys/wait.h>
+
+#include "RachUtils.h"
+
+void ChildProgram( int childToParent[2] )
+{
+    // The child process is going to write to the console,
+    // and that console output will also be the parent's input
+	char message[] = "I hate C strings\n";
+	size_t messageLength = strlen( message ) + 1;
+
+    // Duplicate - Child in << STDOUT
+	close( childToParent[ READ_PIPE ] );
+    dup2( childToParent[ WRITE_PIPE ], STDOUT_FILENO );
+
+	// Send data through the output pipe
+	printf( "CHILD: " );
+	printf( message );
+}
+
+void ParentProgram( int childToParent[2] )
+{
+	char readBuffer[80];
+
+	// Parent process - must close the "output" pipe
+	close( childToParent[ WRITE_PIPE ] );
+
+	// Receive data through the input pipe
+	int byteCount = read( childToParent[ READ_PIPE ], readBuffer, sizeof( readBuffer ) );
+
+	if ( byteCount > 0 )
+	{
+        cout << endl << endl << "PARENT: Received message: \"" << readBuffer << "\" (" << byteCount << " bytes)" << endl;
+	}
+	else
+	{
+        cout << endl << endl << "PARENT: No information received." << endl;
+	}
+}
+
+int main()
+{
+	// Establish a pipeline
+	int childToParent[2];
+	pipe( childToParent );
+
+	// Create a child process to execute a task
+	pid_t childPid = fork();
+	if ( childPid == FORK_ERROR )
+	{
+		cerr << "Error with fork()" << endl;
+		return FORK_ERROR;
+	}
+	else if ( childPid == CHILD_PROCESS )
+	{
+		ChildProgram( childToParent );
+	}
+	else
+	{
+        wait( NULL );
+		ParentProgram( childToParent );
+	}
+
+	return 0;
+}
