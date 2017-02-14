087148526d789f6e9de3496d0233199a83085605
Rachel "Wil Sha" Morris <racheljmorris@gmail.com>
2017-02-14 17:19:58 -0600

    Learning about signals

--- Labs/Lab 04/signals/signals.c
+++ Labs/Lab 04/signals/signals.c
@@ -1,3 +1,5 @@
+/* Rachel J Morris, EECS 678, Lab 4, 2017 */
+
 #include <stdio.h>     /* standard I/O functions                         */
 #include <stdlib.h>    /* exit                                           */
 #include <unistd.h>    /* standard unix functions, like getpid()         */
@@ -6,30 +8,43 @@
 /* first, define the Ctrl-C counter, initialize it with zero. */
 int ctrl_c_count = 0;
 int got_response = 0;
-#define CTRL_C_THRESHOLD  5 
+#define CTRL_C_THRESHOLD  5
+
+struct Signal
+{
+    int count;
+    int gotRepsonse;
+};
 
 /* the Ctrl-C signal handler */
 void catch_int(int sig_num)
 {
   /* increase count, and check if threshold was reached */
   ctrl_c_count++;
-  if (ctrl_c_count >= CTRL_C_THRESHOLD) {
+
+  if (ctrl_c_count >= CTRL_C_THRESHOLD)
+  {
     char answer[30];
 
     /* prompt the user to tell us if to really
      * exit or not */
     printf("\nReally exit? [Y/n]: ");
+
     fflush(stdout);
+
     fgets(answer, sizeof(answer), stdin);
-    if (answer[0] == 'n' || answer[0] == 'N') {
+
+    if ( answer[0] == 'n' || answer[0] == 'N' )
+    {
       printf("\nContinuing\n");
       fflush(stdout);
-      /* 
+      /*
        * Reset Ctrl-C counter
        */
       ctrl_c_count = 0;
     }
-    else {
+    else
+    {
       printf("\nExiting...\n");
       fflush(stdout);
       exit(0);
@@ -45,7 +60,7 @@ void catch_tstp(int sig_num)
   fflush(stdout);
 }
 
-int main(int argc, char* argv[])
+int main( int argc, char* argv[] )
 {
   struct sigaction sa;
   sigset_t mask_set;  /* used to set a signal masking set. */
