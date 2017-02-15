4230766fdfb6b7df2e9335505f6e95f028925a76
Rachel "Wil Sha" Morris <racheljmorris@gmail.com>
2017-02-14 19:05:56 -0600

    Signals basically following proper functionality

--- Labs/Lab 04/signals/signals.c
+++ Labs/Lab 04/signals/signals.c
@@ -1,74 +1,101 @@
 /* Rachel J Morris, EECS 678, Lab 4, 2017 */
 
-#include <stdio.h>     /* standard I/O functions                         */
-#include <stdlib.h>    /* exit                                           */
-#include <unistd.h>    /* standard unix functions, like getpid()         */
-#include <signal.h>    /* signal name macros, and the signal() prototype */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
 
-/* first, define the Ctrl-C counter, initialize it with zero. */
-int ctrl_c_count = 0;
-int got_response = 0;
-#define CTRL_C_THRESHOLD  5
+int terminalStopCounter = 0;
+const int QUIT_INTERRUPT_THRESHOLD = 5;
+const int QUIT_TIMER_THRESHOLD = 10;
 
-struct Signal
+void HandleInterrupt( int signal );
+void HandleTerminalStop( int signal );
+void HandleAlarm();
+void SetupInterruptSignals();
+void SetupAlarmSignal();
+
+int main()
 {
-    int count;
-    int gotRepsonse;
-};
+    SetupInterruptSignals();
 
-/* the Ctrl-C signal handler */
-void catch_int(int sig_num)
+    printf( "Welcome to program. \n " );
+    printf( "Press CTRL+C 5 times to prompt for quit, " );
+    printf( "or CTRL+Z to view tstp counter. \n\n" );
+    fflush( stdout );
+
+    while ( 1 );
+
+    return 0;
+}
+
+void HandleInterrupt( int signal )
 {
-  /* increase count, and check if threshold was reached */
-  ctrl_c_count++;
-
-  if (ctrl_c_count >= CTRL_C_THRESHOLD)
-  {
-    char answer[30];
-
-    /* prompt the user to tell us if to really
-     * exit or not */
-    printf("\nReally exit? [Y/n]: ");
-
-    fflush(stdout);
-
-    fgets(answer, sizeof(answer), stdin);
-
-    if ( answer[0] == 'n' || answer[0] == 'N' )
+    terminalStopCounter++;
+    if ( terminalStopCounter >= QUIT_INTERRUPT_THRESHOLD )
     {
-      printf("\nContinuing\n");
-      fflush(stdout);
-      /*
-       * Reset Ctrl-C counter
-       */
-      ctrl_c_count = 0;
+        printf( "\n\n QUIT interrupt threshold hit! \n" );
+        printf( "Wait %i seconds to quit, or enter 'y' to exit. \n\n", QUIT_TIMER_THRESHOLD );
+
+        // Set up timer that will "log out" after 10 seconds
+        SetupAlarmSignal();
+
+        // Prompt user whether they want to quit
+        char answer[30];
+        printf( "\n Really exit? (y/n): " );
+        fflush( stdout );
+        fgets( answer, sizeof( answer ), stdin );
+
+        if ( tolower( answer[0] ) == 'n' )
+        {
+            printf( "\n Continuing... \n" );
+            fflush( stdout );
+            // Reset counter
+            terminalStopCounter = 0;
+        }
+        else
+        {
+            printf( "\n Exiting... \n" );
+            fflush( stdout );
+            exit( 0 );
+        }
     }
-    else
-    {
-      printf("\nExiting...\n");
-      fflush(stdout);
-      exit(0);
-    }
-  }
+
+    printf( "Interrupt \n" );
 }
 
-/* the Ctrl-Z signal handler */
-void catch_tstp(int sig_num)
+void HandleTerminalStop( int signal )
 {
-  /* print the current Ctrl-C counter */
-  printf("\n\nSo far, '%d' Ctrl-C presses were counted\n\n", ctrl_c_count);
-  fflush(stdout);
+    printf( "\n\n So far, '%d' Ctrl-C presses were counted. \n\n", terminalStopCounter );
+    fflush( stdout );
 }
 
-int main( int argc, char* argv[] )
+void HandleAlarm()
 {
-  struct sigaction sa;
-  sigset_t mask_set;  /* used to set a signal masking set. */
-
-  /* setup mask_set */
-
-  /* set signal handlers */
-
-  return 0;
+    printf( " TIMEOUT! Exiting. \n\n" );
 }
 
+void SetupInterruptSignals()
+{
+    signal( SIGTSTP, HandleTerminalStop );
+    signal( SIGINT, HandleInterrupt );
+}
+
+void SetupAlarmSignal()
+{
+    // Set up HandleAlarm to be the function
+    // to handle the signal
+    struct sigaction signalAction;
+    signalAction.sa_handler = HandleAlarm;
+    sigaction( SIGALRM, &signalAction, NULL );
+
+    // Get mask and unblock
+    //sigfillset( &signalAction.sa_mask );
+
+    sigset_t mask;
+    sigprocmask( 0, NULL, &mask );
+    sigdelset( &mask, SIGALRM );
+
+    //signal( SIGALRM, HandleAlarm );
+    alarm( QUIT_TIMER_THRESHOLD );
+}
