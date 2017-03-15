1b9d73c5c44c33006960e86edf9940e3785d2442
Rachel Morris <rach.morris@ku.edu>
2017-03-15 17:08:15 -0500

    Math operations with function pointers

--- Labs/Lab 06 function pointers/eecs678-funcptr-lab/task2/task2.c
+++ Labs/Lab 06 function pointers/eecs678-funcptr-lab/task2/task2.c
@@ -1,15 +1,71 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* IMPLEMENT ME: Declare your functions here */
+typedef int (*operation_ptr)(int, int);
+
 int add (int a, int b);
+int subtract (int a, int b);
+int multiply (int a, int b);
+int divide (int a, int b);
+
+void GetAandB( int* a, int* b );
+int GetOperation();
 
 int main (void)
 {
-	/* IMPLEMENT ME: Insert your algorithm here */
+	int a, b;
+	int operation;
+	operation_ptr operations[] = { add, subtract, multiply, divide };
+
+	GetAandB( &a, &b );
+	operation = GetOperation();
+
+	int result = operations[ operation ]( a, b );
+
+	printf( "x = %d \n", result );
 
 	return 0;
 }
 
-/* IMPLEMENT ME: Define your functions here */
-int add (int a, int b) { printf ("Adding 'a' and 'b'\n"); return a + b; }
+void GetAandB( int* a, int* b )
+{
+	printf( "Operand 'a' : " );
+	scanf( "%d", a );
+
+	printf( "Operand 'b' : " );
+	scanf( "%d", b );
+}
+
+int GetOperation()
+{
+	int operation;
+
+	printf( "Specify the operation to perform (0 : add | 1 : subtract | 2 : multiply | 3 : divide): ");
+	scanf( "%d", &operation );
+
+	return operation;
+}
+
+int add (int a, int b)
+{
+	printf ("Adding 'a' and 'b'\n");
+	return a + b;
+}
+
+int subtract (int a, int b)
+{
+	printf ("Subtracting 'a' and 'b'\n");
+	return a - b;
+}
+
+int multiply (int a, int b)
+{
+	printf ("Multiplying 'a' and 'b'\n");
+	return a * b;
+}
+
+int divide (int a, int b)
+{
+	printf ("Dividing 'a' and 'b'\n");
+	return a / b;
+}
