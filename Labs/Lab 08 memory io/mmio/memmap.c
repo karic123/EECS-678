800b19b1b022f2b9ac83d5236063e58456406c4f
Rachel "Wil Sha" Morris <racheljmorris@gmail.com>
2017-04-12 13:39:06 -0500

    Memory mappin'

--- Labs/Lab 08 memory io/mmio/memmap.c
+++ Labs/Lab 08 memory io/mmio/memmap.c
@@ -36,8 +36,8 @@ int main (int argc, char *argv[])
   if (argc != 3)
     err_quit ("usage: memmap <fromfile> <tofile>");
 
-  /* 
-   * open the input file 
+  /*
+   * open the input file
    */
   if ((fdin = open (argv[1], O_RDONLY)) < 0) {
     sprintf(buf, "can't open %s for reading", argv[1]);
@@ -45,8 +45,8 @@ int main (int argc, char *argv[])
     exit(errno);
   }
 
-  /* 
-   * open/create the output file 
+  /*
+   * open/create the output file
    */
   if ((fdout = open (argv[2], O_RDWR | O_CREAT | O_TRUNC, 0644)) < 0) {
     sprintf (buf, "can't create %s for writing", argv[2]);
@@ -54,34 +54,77 @@ int main (int argc, char *argv[])
     exit(errno);
   }
 
-  /* 
-   * 1. find size of input file 
+  /*
+   * 1. find size of input file
+   */
+   struct stat fileStatz;
+   if ( fstat( fdin, &fileStatz ) < 0 )
+   {
+    sprintf( buf, "can't find filesize of %s", argv[1] );
+    perror( buf );
+    err_sys( "can't find filesize of input file." );
+    exit( errno );
+   }
+
+   printf( "File size of %s: %d \n", argv[1], fileStatz.st_size );
+
+  /*
+   * 2. go to the location corresponding to the last byte
+   */
+   if ( lseek( fdout, fileStatz.st_size - 1, SEEK_SET ) < 0 )
+   {
+    sprintf( buf, "can't find seek in %s", argv[2] );
+    perror( buf );
+    err_sys( "can't seek on output file!" );
+    exit( errno );
+   }
+
+  /*
+   * 3. write a dummy byte at the last location
+   */
+   if ( write( fdout, " ", 1 ) < 0 )
+   {
+    sprintf( buf, "can't write dummy byte to %s", argv[2] );
+    perror( buf );
+    err_sys( "can't write dummy bite." );
+    exit( errno );
+   }
+
+  /*
+   * 4. mmap the input file
+   */
+   src = mmap( NULL, fileStatz.st_size, PROT_READ, MAP_SHARED, fdin, 0 );
+   if ( src < 0 )
+   {
+        sprintf( buf, "Can't mmap %s", argv[1] );
+        perror( buf );
+        err_sys( "can't mmap input file" );
+        exit( errno );
+   }
+
+  /*
+   * 5. mmap the output file
+   */
+   dst = mmap( NULL, fileStatz.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, fdout, 0 );
+    if ( dst < 0 )
+    {
+        sprintf( buf, "Can't mmap %s", argv[2] );
+        perror( buf );
+        err_sys( "can't mmap output file" );
+        exit( errno );
+    }
+
+  /*
+   * 6. copy the input file to the output file
    */
 
-  /* 
-   * 2. go to the location corresponding to the last byte 
-   */
+   memcpy( dst, src, fileStatz.st_size );
 
-  /* 
-   * 3. write a dummy byte at the last location 
-   */
-
-  /* 
-   * 4. mmap the input file 
-   */
-
-  /* 
-   * 5. mmap the output file 
-   */
-
-  /* 
-   * 6. copy the input file to the output file 
-   */
     /* Memory can be dereferenced using the * operator in C.  This line
      * stores what is in the memory location pointed to by src into
      * the memory location pointed to by dest.
      */
     *dst = *src;
-} 
+}
 
 
